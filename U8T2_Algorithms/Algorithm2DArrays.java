3485c39b9dc4e577bb29b3dcc2ca59fdfb73924f
BrightonYew <brightony@nycstudents.net>
2025-03-07 13:09:28 +0000

    a

---
+++ U8T2_Algorithms/Algorithm2DArrays.java
@@ -0,0 +1,15 @@
+public class Algorithm2DArrays {
+    
+    /** Return the sum of all elements of arr that are in the one particular row, 
+     *  specified by the row parameter
+     *
+     *  PRECONDITION:  0 <= row < arr.length (i.e. row is guaranteed to be valid)
+     *
+     *  @param arr  2D array of ints
+     *  @param row  the row to add up
+     *  @return  the sum of all elements in row
+     
+     */
+    public static int sumForRow(int[][] arr, int row)
+    { /* implement this method */ }
+  
